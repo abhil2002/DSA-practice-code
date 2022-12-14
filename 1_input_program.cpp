57811504d20a2a3c01348c69c0b5e24a8dde8ab9
abhi2002 <abhisheklawhale2002@gmail.com>
2022-12-14 12:23:42 +0530

    Second day

---
+++ 1_input_program.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    int amount1;
+    cin>>amount1;
+
+    int amount2;
+    cin>>amount2;
+
+    int amount3;
+    cin>>amount3;
+
+    int sum;
+    sum = amount1+amount2+amount3;
+    cout<<"Sum = "<<sum;
+
+    return 0;
+
+
+
+
+}
